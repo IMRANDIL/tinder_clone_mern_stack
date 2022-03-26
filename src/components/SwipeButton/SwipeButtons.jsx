a3c39ee34c9501a5648fbd4c9ea579721a8b63bf
IMRANDIL <aliimranadil2@gmail.com>
2022-03-26 09:30:31 +0530

    yh

--- src/components/SwipeButton/SwipeButtons.jsx
+++ src/components/SwipeButton/SwipeButtons.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
 import './SwipeButtons.css'
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import { IconButton } from '@mui/material';
+
+
+
+
+
+
+
 
 
 
@@ -8,7 +21,30 @@ import './SwipeButtons.css'
 
 const SwipeButtons = () => {
     return (
-        <div>SwipeButtons</div>
+        <div className='swipeButtons'>
+
+            <IconButton className='swipeButtons__repeat'>
+                <ReplayIcon fontSize='large' />
+            </IconButton>
+
+            <IconButton className='swipeButtons__left'>
+                <CloseIcon fontSize='large' />
+            </IconButton>
+
+            <IconButton className='swipeButtons__star'>
+                <StarRateIcon fontSize='large' />
+            </IconButton>
+
+            <IconButton className='swipeButtons__right'>
+                <FavoriteIcon fontSize='large' />
+            </IconButton>
+
+            <IconButton className='swipeButtons__lightning'>
+                <FlashOnIcon fontSize='large' />
+            </IconButton>
+
+
+        </div>
     )
 }
 
