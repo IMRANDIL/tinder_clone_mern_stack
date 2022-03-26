5ce62983225b98aca5f126682c54f05ee080d221
IMRANDIL <aliimranadil2@gmail.com>
2022-03-26 07:30:08 +0530

    yh

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 import './Header.css'
+import PersonIcon from '@mui/icons-material/Person';
+import { IconButton } from '@mui/material';
+
+
+
+
 
 
 const Header = () => {
     return (
         <div className='header'>
+            <IconButton>
+                <PersonIcon fontSize='large' className='header__icon' />
+            </IconButton>
+
+            <img draggable='false' src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon.png" className='header__logo' alt="tinder-img" />
+
 
         </div>
     )
