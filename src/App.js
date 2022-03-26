cdd924ca319eaeef0cbd4ba065f5f6b20b7c520b
IMRANDIL <aliimranadil2@gmail.com>
2022-03-26 07:15:12 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="app">
-      hey tinder
+
+
+
+
+
+      {/* Header */}
+
+      <Header />
+
+      {/* Tinder Card */}
+
+
+
+      {/* buttons-header */}
     </div>
   );
 }
